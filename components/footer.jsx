df91bdcfeaf7743f5e53322ae789e08bbb20dccb
adnankunjoo <adnankunjoo00@gmail.com>
2025-10-06 15:01:37 +0530

    Initial commit

---
+++ components/footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return(
+        <div id="footer">
+        <footer>
+            <h2>Contacts</h2>
+            <p>Email: alyan@gmail.com</p>
+            <p>Phone No: 12345-67890</p>
+        </footer>
+        </div>
+    )
+}
---
+++ components/menu.jsx
@@ -0,0 +1,14 @@
+export default function Menu() {
+    return(
+        <div id="menu">
+        <section>
+            <h2>Menu</h2>
+            <ul>
+                <li>Kebab</li>
+                <li>Shawarma</li>
+                <li>Madhood</li>
+            </ul>
+        </section>
+        </div>
+    )
+}
